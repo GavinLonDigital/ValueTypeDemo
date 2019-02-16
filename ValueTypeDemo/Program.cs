464e2610e5ae3856a95c8c850be87e92c8bdf97d
Lon <gavinlondee@gmail.com>
2019-02-16 10:02:56 +0200

    Initial commit

---
+++ ValueTypeDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ValueTypeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal loanAmount = 0;
+            decimal loanAmountCopy = 0;
+
+            Console.WriteLine("Please enter a loan amount");
+
+            loanAmount = Convert.ToDecimal(Console.ReadLine());
+
+            loanAmountCopy = loanAmount;
+
+            Console.WriteLine($"loanAmount: {loanAmount}, loanAmountCopy {loanAmountCopy}");
+
+            Console.ReadKey();
+
+            loanAmount = loanAmount + 600;
+
+            Console.WriteLine();
+            Console.WriteLine($"After adding 600 pounds loanAmount: {loanAmount}, loanAmountCopy {loanAmountCopy}");
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
